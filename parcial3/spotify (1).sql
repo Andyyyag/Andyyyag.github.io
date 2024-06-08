22f11e96f35589fa40923893f41429caad680a29
Andyyyag <159413608+Andyyyag@users.noreply.github.com>
2024-06-07 18:25:21 -0700

    practica 8 04/06/24

--- parcial3/spotify (1).sql
+++ parcial3/spotify (1).sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2024 a las 04:38:52
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 08-06-2024 a las 03:24:25
+-- Versión del servidor: 10.1.39-MariaDB
+-- Versión de PHP: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,7 +32,7 @@ CREATE TABLE `artistas_y_canciones` (
   `id` int(11) NOT NULL,
   `artistas_id` int(11) NOT NULL,
   `cancion_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -43,7 +44,7 @@ CREATE TABLE `artstas` (
   `id` int(11) NOT NULL,
   `nombre` varchar(1000) NOT NULL,
   `descripcion` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -59,7 +60,17 @@ CREATE TABLE `canciones` (
   `fecha` date NOT NULL,
   `activo` tinyint(1) NOT NULL,
   `foto` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `canciones`
+--
+
+INSERT INTO `canciones` (`id`, `nombre`, `genero_id`, `duracion`, `fecha`, `activo`, `foto`) VALUES
+(1, 'Attention', 6, '00:03:17', '2025-11-16', 1, 'STARBOY\r\nTHE WEEKND\r\ncolores rojos y azules'),
+(2, 'drivers license', 2, '00:04:02', '2021-05-21', 1, 'fondo lila\r\nolivia'),
+(3, 'R U MINE?', 3, '00:03:21', '2013-09-09', 1, 'onda de sonido\r\nfondo negro'),
+(4, 'James', 4, '00:02:55', '2021-04-30', 1, 'Laufey');
 
 -- --------------------------------------------------------
 
@@ -69,8 +80,8 @@ CREATE TABLE `canciones` (
 
 CREATE TABLE `genero` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `nombre` varchar(150) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `genero`
@@ -94,9 +105,9 @@ INSERT INTO `genero` (`id`, `nombre`) VALUES
 
 CREATE TABLE `membresia` (
   `id1` int(11) NOT NULL,
-  `descripcion` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `descripcion` varchar(100) CHARACTER SET utf8 NOT NULL,
   `precio` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `membresia`
@@ -119,7 +130,7 @@ CREATE TABLE `playlist` (
   `total_can` int(11) NOT NULL,
   `descripcion` varchar(200) NOT NULL,
   `publico` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -132,7 +143,7 @@ CREATE TABLE `playlist_canciones` (
   `playlist_id` int(11) NOT NULL,
   `cancion_id` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -145,7 +156,7 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(1000) NOT NULL,
   `correo` varchar(300) NOT NULL,
   `membresias_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
@@ -156,7 +167,9 @@ CREATE TABLE `usuarios` (
 --
 ALTER TABLE `artistas_y_canciones`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
+  ADD KEY `id` (`id`),
+  ADD KEY `artistas_id` (`artistas_id`),
+  ADD KEY `cancion_id` (`cancion_id`);
 
 --
 -- Indices de la tabla `artstas`
@@ -170,7 +183,8 @@ ALTER TABLE `artstas`
 --
 ALTER TABLE `canciones`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
+  ADD KEY `id` (`id`),
+  ADD KEY `genero_id` (`genero_id`);
 
 --
 -- Indices de la tabla `genero`
@@ -196,7 +210,10 @@ ALTER TABLE `playlist`
 --
 ALTER TABLE `playlist_canciones`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
+  ADD KEY `id` (`id`),
+  ADD KEY `playlist_id` (`playlist_id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `cancion_id` (`cancion_id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -213,6 +230,31 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `genero`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `artistas_y_canciones`
+--
+ALTER TABLE `artistas_y_canciones`
+  ADD CONSTRAINT `artistas_y_canciones_ibfk_1` FOREIGN KEY (`artistas_id`) REFERENCES `artstas` (`id`),
+  ADD CONSTRAINT `artistas_y_canciones_ibfk_2` FOREIGN KEY (`cancion_id`) REFERENCES `canciones` (`id`);
+
+--
+-- Filtros para la tabla `canciones`
+--
+ALTER TABLE `canciones`
+  ADD CONSTRAINT `canciones_ibfk_1` FOREIGN KEY (`genero_id`) REFERENCES `genero` (`id`);
+
+--
+-- Filtros para la tabla `playlist_canciones`
+--
+ALTER TABLE `playlist_canciones`
+  ADD CONSTRAINT `playlist_canciones_ibfk_1` FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`id`),
+  ADD CONSTRAINT `playlist_canciones_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `playlist_canciones_ibfk_3` FOREIGN KEY (`cancion_id`) REFERENCES `canciones` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
