0319397b77bea970e00af0f5022eab775542a2fa
Andyyyag <159413608+Andyyyag@users.noreply.github.com>
2024-11-05 08:36:17 -0700

    Delete parcial02 directory

--- parcial02/practica_1.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("boton1").addEventListener("click", function(){
-    document.getElementById("nombre").style.color="Blue";
-});
-document.getElementById("boton2").addEventListener("click", function(){
-    document.getElementById("nombre").style.fontSize="100px";
-});
-document.getElementById("boton3").addEventListener("click", function(){
-    document.getElementById("nombre").style.display="none";
-});
